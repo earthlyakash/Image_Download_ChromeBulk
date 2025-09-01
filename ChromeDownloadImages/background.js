7e64e0840d95f02f4a4a1f2104b89154180c32bf
earthlyakash <111906266+earthlyakash@users.noreply.github.com>
2025-09-01 12:33:14 +0530

    Delete ChromeDownloadImages/background.js

--- ChromeDownloadImages/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.url) {
-    chrome.downloads.download({
-      url: message.url,
-      conflictAction: 'uniquify',
-      saveAs: false
-    });
-  }
-});
